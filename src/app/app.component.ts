c6c1426b90ad94a668b1ab286880ffe5ecc11e70
Paul Calimache <paul.calimache@gmail.com>
2022-05-20 08:44:01 +0000

    display arrow depending route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio';
+
+  constructor(private route: Router) { }
+  
+  public title: string = 'portfolio';
+
+  public get IsHomePage() {
+    console.log(this.route.url)
+    return this.route.url === '/';
+  }
+
 }
